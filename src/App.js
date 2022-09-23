595e9780cdc3ebaab36964fc2c5f543faddf5aff
anchal <anchalchouhan085@gmail.com>
2022-09-23 15:23:49 +0530

    initei change

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
-import Nav from './components/Nav';
-import './App.css';
-import Card from './components/Card';
-import Table from './components/Table';
-import DateRang from './components/DateRang';
-import SecondCard from './components/SecondCard';
-import ThirdCard from './components/ThirdCard';
+import Nav from "./components/Nav";
+import "./App.css";
+import Card from "./components/Card";
+import Table from "./components/Table";
+import DateRang from "./components/DateRang";
+import SecondCard from "./components/SecondCard";
+import ThirdCard from "./components/ThirdCard";
+import ChartBox from "./ChartBox";
 
 function App() {
   return (
     <div className="App">
-     <Nav></Nav>
-     <h2 className='first-let' >Total Calls</h2>
-     <h2 className='second-let'>Highest Call Time</h2>
-
-     <div className='container122'>
-
-<Card sign='Active calls' num='7' link ='View All Active Calls'/>
-<Card sign='Customers in Queue' num='26' link ='View All Queue'/>
-
-
-
-<div>
-  <Table></Table>
-</div>
- </div>
-<div>
-  <DateRang/>
-</div>
-<div>
-<SecondCard sign='Customers in Queue' num='26' link ='View All Queue'/>
-</div>
-<div>
-  <ThirdCard></ThirdCard>
-</div>
+      <Nav></Nav>
+      <h2 className="first-let">Total Calls</h2>
+      <h2 className="second-let">Highest Call Time</h2>
+      
+      <div className="container122">
+      <div className="card1222">
+      <Card sign= "Active calls" num="7" link="View All Active Calls" />
+        <Card sign="Customers in Queue" num="26" link="View All Queue" />
+        <ChartBox/>
+      </div>
+      
+        <Table></Table>
+    
+      </div>
+      <div>
+        <DateRang />
+      </div>
+      <div>
+        <SecondCard sign="Customers in Queue" num="26" link="View All Queue" />
+      </div>
+      <div>
+        <ThirdCard></ThirdCard>
+      </div>
     </div>
   );
 }
--- src/components/ThirdCard.js
+++ src/components/ThirdCard.js
@@ -1,18 +1,22 @@
-import './ThirdCard.css';
+import "./ThirdCard.css";
 
-function ThirdCard(){
-    return(
-        <>
-            <div className='t-2'>
-                <div className='lobbycard'>
-                    <span className='span3'>Lobby drop off </span>
-                    <br/>
-                    <h2 className='percent'>7.5%</h2>
-                </div>
-                <div className='graphCard'></div>
-            </div>
-        </>
-    );
+
+function ThirdCard() {
+  return (
+    <>
+      <div className="t-2">
+        <div className="lobbycard">
+          <span className="span3">Lobby drop off </span>
+          <br />
+          <h2 className="percent">7.5%</h2>
+        </div>
+        <div className="graphCard-container">
+          <div className="graphCard-child1">1</div>
+          <div className="graphCard-child2">2</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ThirdCard;
